409f8e713e4fc4fedbc1e9169abf9c7d09fa43f3
Jochen Fürbacher <jochen.fuerbacher@1und1.de>
2017-11-30 15:59:37 +0100

    Reformat class.

--- src/main/java/hudson/plugins/textfinder/TextFinderPublisher.java
+++ src/main/java/hudson/plugins/textfinder/TextFinderPublisher.java
@@ -1,29 +1,10 @@
 package hudson.plugins.textfinder;
 
-import static hudson.Util.fixEmpty;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.Serializable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
-
-import javax.servlet.ServletException;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.tools.ant.DirectoryScanner;
-import org.apache.tools.ant.types.FileSet;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-
-import hudson.Extension;
 import hudson.FilePath.FileCallable;
 import hudson.Launcher;
 import hudson.Util;
+import hudson.Extension;
+import static hudson.Util.fixEmpty;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
@@ -36,6 +17,22 @@ import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Publisher;
 import hudson.tasks.Recorder;
 import hudson.util.FormValidation;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.types.FileSet;
+import org.apache.commons.io.IOUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import javax.servlet.ServletException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * Text Finder plugin for Jenkins. Search in the workspace using a regular 
@@ -75,150 +72,136 @@ public class TextFinderPublisher extends Recorder implements Serializable {
     }
 
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
-    	findText(build, listener.getLogger());
+        findText(build, listener.getLogger());
         return true;
     }
-    
-	/**
-	 * Indicates an orderly abortion of the processing.
-	 */
-	private static final class AbortException extends RuntimeException {
-	}
-    
-	protected void findText(Run<?, ?> run, PrintStream logger)
-			throws IOException, InterruptedException {
-		try {
-			boolean foundText = false;
 
-			if (alsoCheckConsoleOutput) {
-				logger.println("Checking console output");
-				foundText |= checkFile(run.getLogFile(), compilePattern(logger),
-						logger, true);
-			} else {
-				// printing this when checking console output will cause the
-				// plugin
-				// to find this line, which would be pointless.
-				// doing this only when fileSet!=null to avoid
-				logger.println("Checking " + regexp);
-			}
+    /**
+     * Indicates an orderly abortion of the processing.
+     */
+    private static final class AbortException extends RuntimeException {
+    }
 
-			final RemoteOutputStream ros = new RemoteOutputStream(logger);
+    protected void findText(Run<?, ?> run, PrintStream logger) throws IOException, InterruptedException {
+        try {
+            boolean foundText = false;
 
-			if (fileSet != null && run instanceof AbstractBuild<?, ?>) {
-				AbstractBuild<?, ?> build = (AbstractBuild<?, ?>) run;
-				foundText |= build.getWorkspace()
-						.act(new FileCallable<Boolean>() {
-							public Boolean invoke(File ws,
-									VirtualChannel channel) throws IOException {
-								PrintStream logger = new PrintStream(ros);
+            if(alsoCheckConsoleOutput) {
+                logger.println("Checking console output");
+                foundText |= checkFile(run.getLogFile(), compilePattern(logger), logger, true);
+            } else {
+                // printing this when checking console output will cause the plugin
+                // to find this line, which would be pointless.
+                // doing this only when fileSet!=null to avoid
+                logger.println("Checking " + regexp);
+            }
 
-								// Collect list of files for searching
-								FileSet fs = new FileSet();
-								org.apache.tools.ant.Project p = new org.apache.tools.ant.Project();
-								fs.setProject(p);
-								fs.setDir(ws);
-								fs.setIncludes(fileSet);
-								DirectoryScanner ds = fs.getDirectoryScanner(p);
+            final RemoteOutputStream ros = new RemoteOutputStream(logger);
 
-								// Any files in the final set?
-								String[] files = ds.getIncludedFiles();
-								if (files.length == 0) {
-									logger.println(
-											"Jenkins Text Finder: File set '"
-													+ fileSet + "' is empty");
-									throw new AbortException();
-								}
+            if(fileSet!=null && run instanceof AbstractBuild<?, ?>) {
+            	AbstractBuild<?, ?> build = (AbstractBuild<?, ?>) run;
+                foundText |= build.getWorkspace().act(new FileCallable<Boolean>() {
+                    public Boolean invoke(File ws, VirtualChannel channel) throws IOException {
+                        PrintStream logger = new PrintStream(ros);
 
-								Pattern pattern = compilePattern(logger);
+                        // Collect list of files for searching
+                        FileSet fs = new FileSet();
+                        org.apache.tools.ant.Project p = new org.apache.tools.ant.Project();
+                        fs.setProject(p);
+                        fs.setDir(ws);
+                        fs.setIncludes(fileSet);
+                        DirectoryScanner ds = fs.getDirectoryScanner(p);
 
-								boolean foundText = false;
+                        // Any files in the final set?
+                        String[] files = ds.getIncludedFiles();
+                        if (files.length == 0) {
+                            logger.println("Jenkins Text Finder: File set '" +
+                                    fileSet + "' is empty");
+                            throw new AbortException();
+                        }
 
-								for (String file : files) {
-									File f = new File(ws, file);
+                        Pattern pattern = compilePattern(logger);
 
-									if (!f.exists()) {
-										logger.println(
-												"Jenkins Text Finder: Unable to"
-														+ " find file '" + f
-														+ "'");
-										continue;
-									}
-									if (!f.canRead()) {
-										logger.println(
-												"Jenkins Text Finder: Unable to"
-														+ " read from file '"
-														+ f + "'");
-										continue;
-									}
+                        boolean foundText = false;
 
-									foundText |= checkFile(f, pattern, logger,
-											false);
-								}
+                        for (String file : files) {
+                            File f = new File(ws, file);
 
-								return foundText;
-							}
-						});
-			}
+                            if (!f.exists()) {
+                                logger.println("Jenkins Text Finder: Unable to" +
+                                    " find file '" + f + "'");
+                                continue;
+                            }
+                            if (!f.canRead()) {
+                                logger.println("Jenkins Text Finder: Unable to" +
+                                    " read from file '" + f + "'");
+                                continue;
+                            }
 
-			if (foundText != succeedIfFound)
-				run.setResult(
-						unstableIfFound ? Result.UNSTABLE : Result.FAILURE);
-		} catch (AbortException e) {
-			// no test file found
-			run.setResult(Result.UNSTABLE);
-		}
-	}
+                            foundText |= checkFile(f, pattern, logger, false);
+                        }
 
-	/**
-	 * Search the given regexp pattern in the file.
-	 *
-	 * @param abortAfterFirstHit
-	 *            true to return immediately as soon as the first hit is found.
-	 *            this is necessary when we are scanning the console output,
-	 *            because otherwise we'll loop forever.
-	 */
-	private boolean checkFile(File f, Pattern pattern, PrintStream logger,
-			boolean abortAfterFirstHit) {
-		boolean logFilename = true;
-		boolean foundText = false;
-		BufferedReader reader = null;
-		try {
-			// Assume default encoding and text files
-			String line;
-			reader = new BufferedReader(new FileReader(f));
-			while ((line = reader.readLine()) != null) {
-				Matcher matcher = pattern.matcher(line);
-				if (matcher.find()) {
-					if (logFilename) {// first occurrence
-						logger.println(f + ":");
-						logFilename = false;
-					}
-					logger.println(line);
-					foundText = true;
-					if (abortAfterFirstHit)
-						return true;
-				}
-			}
-		} catch (IOException e) {
-			logger.println("Jenkins Text Finder: Error reading" + " file '" + f
-					+ "' -- ignoring");
-		} finally {
-			IOUtils.closeQuietly(reader);
-		}
-		return foundText;
-	}
+                        return foundText;
+                    }
+                });
+            }
 
-	private Pattern compilePattern(PrintStream logger) {
-		Pattern pattern;
-		try {
-			pattern = Pattern.compile(regexp);
-		} catch (PatternSyntaxException e) {
-			logger.println("Jenkins Text Finder: Unable to compile"
-					+ "regular expression '" + regexp + "'");
-			throw new AbortException();
-		}
-		return pattern;
-	}
+            if (foundText != succeedIfFound)
+            	run.setResult(unstableIfFound ? Result.UNSTABLE : Result.FAILURE);
+        } catch (AbortException e) {
+            // no test file found
+        	run.setResult(Result.UNSTABLE);
+        }
+    }
+
+    /**
+     * Search the given regexp pattern in the file.
+     *
+     * @param abortAfterFirstHit
+     *      true to return immediately as soon as the first hit is found. this is necessary
+     *      when we are scanning the console output, because otherwise we'll loop forever. 
+     */
+    private boolean checkFile(File f, Pattern pattern, PrintStream logger, boolean abortAfterFirstHit) {
+        boolean logFilename = true;
+        boolean foundText = false;
+        BufferedReader reader=null;
+        try {
+            // Assume default encoding and text files
+            String line;
+            reader = new BufferedReader(new FileReader(f));
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    if (logFilename) {// first occurrence
+                        logger.println(f + ":");
+                        logFilename = false;
+                    }
+                    logger.println(line);
+                    foundText = true;
+                    if(abortAfterFirstHit)
+                        return true;
+                }
+            }
+        } catch (IOException e) {
+            logger.println("Jenkins Text Finder: Error reading" +
+                " file '" + f + "' -- ignoring");
+        } finally {
+            IOUtils.closeQuietly(reader);
+        }
+        return foundText;
+    }
+
+    private Pattern compilePattern(PrintStream logger) {
+        Pattern pattern;
+        try {
+            pattern = Pattern.compile(regexp);
+        } catch (PatternSyntaxException e) {
+            logger.println("Jenkins Text Finder: Unable to compile"
+                    + "regular expression '" + regexp + "'");
+            throw new AbortException();
+        }
+        return pattern;
+    }
 
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
